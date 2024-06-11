05b62c0b6be6e9e5f33c65d6dfbfb9d4c85c9a20
masukomi <masukomi@masukomi.org>
2024-06-11 13:41:16 -0400

    removed simplecov usage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 require 'rubygems'
 require 'bundler'
 require 'json'
-require 'simplecov'
 require 'grocer'
 require 'debug'
-SimpleCov.start
 
 Dir['lib/passbook/**/*.rb'].each {|f| require File.join(File.dirname(__FILE__), '..', f.gsub(/.rb/, ''))}
 Dir['lib/rack/**/*.rb'].each {|f| require File.join(File.dirname(__FILE__), '..', f.gsub(/.rb/, ''))}
